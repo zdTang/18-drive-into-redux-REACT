a430fabd8f31e982b51927d03739ab356fcac6f9
Zhendong Tang <zhendongtt@gmail.com>
2021-12-03 18:14:04 -0500

    18-005 Exploring The Core Redux Concepts
     Why the initial state is always run automatically?
     All subscribers get the same state.

--- 18-1-Redux-under-nutshell/redux-under-hood.js
+++ 18-1-Redux-under-nutshell/redux-under-hood.js
@@ -4,21 +4,30 @@ const redux = require("redux");
 //inputs:  old state + Dispatched Action
 const counterReducer = (state = { counter: 0 }, action) => {
   return {
-    counter: state + 1, // based on the old state
+    counter: state.counter + 1, // based on the old state
   };
 };
 
 //inputs: reducer function
 const store = redux.createStore(counterReducer);
 
-console.log(store.getState());
+//always get the initial state ?
+console.log("init --", store.getState());
 
 // Define a  Subscriber of the Redux?
 const counterSubscriber = () => {
   const latestState = store.getState();
-  console.log(latestState);
+  console.log("counterSubscriber", latestState);
+};
+// Define another subscriber
+const AnotherCounterSubscriber = () => {
+  const latestState = store.getState();
+  console.log("AnotherCounterSubscriber", latestState);
 };
 
 // Make the redux to aware who is subscribing it
 
 store.subscribe(counterSubscriber);
+store.subscribe(AnotherCounterSubscriber);
+
+store.dispatch({ type: "increment" });
