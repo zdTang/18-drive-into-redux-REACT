60f5d2a06c9161b0e9c035b90863c67174e1bddc
Zhendong Tang <zhendongtt@gmail.com>
2021-12-03 20:31:04 -0500

    18-008 Creating a Redux Store for React
    Create a store and export it
    
    only need a Reducer to initialize a Store

---
+++ src/store/index.js
@@ -0,0 +1,34 @@
+//import redux from "redux";
+import { createStore } from "redux";
+
+// define reducer
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  return state;
+};
+
+// use reducer to init Redux store
+const store = createStore(counterReducer);
+
+export default store;
+
+/*==========  the following lines are supposed to be used in components
+
+//  define subscriber
+const counterSubscriber = () => {
+  const latestState = store.getState();
+};
+
+// bind store with subscriber
+store.subscribe(counterSubscriber);
+
+// store dispatch Action
+store.dispatch({ type: "increment" });
+
+
+
+*/
