b31ab7cfa9fa34e9ccb9b1a8072e7291d54852af
Zhendong Tang <zhendongtt@gmail.com>
2021-12-03 20:59:53 -0500

    18-010 Using Redux Data in React Components

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,12 @@ const counterReducer = (state = { counter: 0 }, action) => {
       counter: state.counter + 1,
     };
   }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
   return state;
 };
 
