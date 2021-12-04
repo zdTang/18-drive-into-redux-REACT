de22281ce030ebd3da474b1a7da802adfbcd2618
Zhendong Tang <zhendongtt@gmail.com>
2021-12-03 19:32:49 -0500

    18-005 Exploring The Core Redux Concepts-3
    Setup the  condition based on the Action.
    
    Be aware,  the initial value will always run  by default.
    we can setup it is equal to the original state if no action is passed in

--- 18-1-Redux-under-nutshell/redux-under-hood.js
+++ 18-1-Redux-under-nutshell/redux-under-hood.js
@@ -4,15 +4,18 @@ const redux = require("redux");
 //inputs:  old state + Dispatched Action
 const counterReducer = (state = { counter: 0 }, action) => {
   console.log("action", action);
-  return {
-    counter: state.counter + 1, // based on the old state
-  };
+  if (action.type == "increment") {
+    return {
+      counter: state.counter + 1, // based on the old state
+    };
+  }
+  return state; //  By now, the initial state will return the original state
 };
 
 //inputs: reducer function
 const store = redux.createStore(counterReducer);
 
-//always get the initial state ?
+//Reducer will setup the logic how to return the initial state
 console.log("init --", store.getState());
 
 // Define a  Subscriber of the Redux?
